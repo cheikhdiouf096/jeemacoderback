b9e514b62f8360d1e8c5c08d008230ff3ee41b22
Salioungom <ngom.saliou4@ugb.edu.sn>
2024-07-04 15:56:04 +0000

    register and login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\UserController;
+
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +21,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Route::middleware([ 'auth:sanctum', EnsureFrontendRequestsAreStateful::class,])->get('/user', function (Request $request) {
+//      return $request->user();
+
+// });
+
+
+// Route::post('/register',[OrganisateurController::class,'register']);
+// Route::post('/login',[OrganisateurController::class,'login']);
+
+
+Route::post('/register',[UserController::class,'register']);
+Route::post('/login',[UserController::class,'login']);
